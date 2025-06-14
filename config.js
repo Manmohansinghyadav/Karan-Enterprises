4cf586d283dee17f258d28e7bef9a723c6a9ccc9
Manmohan Singh <manmohanyadav616668@gmail.com>
2025-06-14 18:54:38 +0530

    paid add

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxA8p5FW01AFUx2li8AqZYxKgiqwntZ3_MPHTsmW06KK1tQ0AyvN-_ef5l8XoFpNZp90w/exec'
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxGXdu9K_njbEkpeSKCPalEtlVQHYgDlk3D9JX-kssx3M8BXeWYcyXlLEMZV1xQckQU6Q/exec'
 };
